0d9e7ab7838a33f22aa4b4ea7f5c5c5ae1340a44
Nathaniel Toju <nathaniel@resopot.com>
2024-08-29 23:03:30 +0100

    remove config.php

--- config-dist.php
+++ config-dist.php
@@ -161,7 +161,8 @@ $CFG->dboptions = array(
 // If you need both intranet and Internet access please read
 // http://docs.moodle.org/en/masquerading
 
-$CFG->wwwroot = 'http://example.com/moodle';
+// $CFG->wwwroot = 'http://example.com/moodle';
+$CFG->wwwroot = 'https://moodle-app-tkj0.onrender.com';
 
 //=========================================================================
 // 3. DATA FILES LOCATION
@@ -176,7 +177,8 @@ $CFG->wwwroot = 'http://example.com/moodle';
 //
 // - On Windows systems you might specify something like 'c:\moodledata'
 
-$CFG->dataroot = '/home/example/moodledata';
+// $CFG->dataroot = '/home/example/moodledata';
+$CFG->dataroot = '/var/www/html';
 
 //=========================================================================
 // 4. DATA FILES PERMISSIONS
